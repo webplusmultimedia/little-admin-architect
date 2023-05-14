47d6394bc9c735f5c08516258838aac002a2cf15
daniel RMC <contact@webplusm.net>
2023-05-13 22:17:24 -0400

    add : laravel mix css style tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require("tailwindcss/colors");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './resources/views/**/*.blade.php',
+      './resources/js/**/*.js',
+  ],
+  theme: {
+    extend: {
+        colors: {
+            'primary-datepicker' : colors.cyan,
+            primary : colors.emerald,
+            secondary : colors.lime,
+            info : colors.indigo,
+            error : colors.red,
+            warning : colors.yellow,
+            success : colors.emerald,
+        }
+    },
+  },
+  plugins: [],
+}
+
