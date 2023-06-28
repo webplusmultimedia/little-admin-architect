7fdf8673f990d9f840bceb6118097b4dcf43afd8
webplusmultimedia <webplusmultimedia@users.noreply.github.com>
2023-06-28 19:42:07 +0000

    Fix styling

--- src/Admin/Livewire/Pages/ListRecord.php
+++ src/Admin/Livewire/Pages/ListRecord.php
@@ -7,6 +7,7 @@ namespace Webplusmultimedia\LittleAdminArchitect\Admin\Livewire\Pages;
 use Illuminate\Support\Str;
 use Illuminate\View\View;
 use Webplusmultimedia\LittleAdminArchitect\Admin\Livewire\Page;
+use Webplusmultimedia\LittleAdminArchitect\Admin\Resources;
 use Webplusmultimedia\LittleAdminArchitect\Form\Components\Form;
 
 class ListRecord extends Page
@@ -25,7 +26,7 @@ class ListRecord extends Page
 
     public static function getForm(): Form
     {
-        /** @var resource $resource */
+        /** @var Resources\Resource $resource */
         $resource = static::getResource();
         static::$form = $resource::getFormSchema(Form::make());
 
