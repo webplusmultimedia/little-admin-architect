b34e60320fc5786abac6664119bbd104a14e6b82
daniel <contact@webplusm.net>
2023-07-31 21:03:05 -0400

    fix : image style column

--- resources/views/table-components/columns/image.blade.php
+++ resources/views/table-components/columns/image.blade.php
@@ -2,7 +2,7 @@
 @php /**@var ImageColumn $column */
 @endphp
 <td wire:key="{{$column->getWireId()}}">
-    <div class="flex  items-center justify-center  min-w-max text-gray-800">
+    <div class="flex  items-center justify-start  min-w-max text-gray-800">
         @if($column->getState())
             <span><img src="{{ (Croppa::url($column->getUrlForCroppa(),60,50)) }}" alt="image" class="object-cover w-12"></span>
         @endif
